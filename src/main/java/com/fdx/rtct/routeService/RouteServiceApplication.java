2e1e5984a6be18aba8533d11c5d02e72256ec752
vijay-navarkar <vijay.a.navarkar@gmail.com>
2020-02-24 05:07:33 +0530

    changes for adding new code

--- src/main/java/com/fdx/rtct/routeService/RouteServiceApplication.java
+++ src/main/java/com/fdx/rtct/routeService/RouteServiceApplication.java
@@ -2,24 +2,26 @@ package com.fdx.rtct.routeService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
 @RequestMapping
 public class RouteServiceApplication {
 	
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println("--allocation started-----");
 		SpringApplication.run(RouteServiceApplication.class, args);
 	}
 	
-	@RequestMapping(path = "/method2" , method = RequestMethod.GET )
-	public String method(){
-		return "Scan Test Controller 2";
-		
-	}
 	
 	
 }
